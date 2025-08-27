e8efc4afdb3a9ddb97c6630521cefaff1af049e1
Bugrova Irina <bugrova147@yandex.ru>
2025-08-27 21:29:33 +0300

    a check has been implemented to ensure that the video link does not contain any resources other than youtube.com

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,16 +1,26 @@
+from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
-from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
+from materials.validators import YoutubeURLValidator
 
 
-class LessonSerializer(ModelSerializer):
+class LessonSerializer(serializers.ModelSerializer):
+    # Явно определяем поле video с валидатором
+    video = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        allow_null=True,
+        validators=[YoutubeURLValidator()],
+        max_length=350,
+    )
+
     class Meta:
         model = Lesson
         fields = "__all__"
 
 
-class CourseSerializer(ModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
     # Вывод информации об уроке
     lesson_info = LessonSerializer(many=True, source="lessons", read_only=True)
 
@@ -19,7 +29,7 @@ class CourseSerializer(ModelSerializer):
         fields = "__all__"
 
 
-class CourseDetailSerializer(ModelSerializer):
+class CourseDetailSerializer(serializers.ModelSerializer):
     count_lessons_into_the_course = SerializerMethodField()
     # Вывод информации об уроке
     lesson_info = LessonSerializer(many=True, source="lessons", read_only=True)
---
+++ materials/validators.py
@@ -0,0 +1,25 @@
+from rest_framework.serializers import ValidationError
+
+
+class YoutubeURLValidator:
+    """
+    Проверка на присутствие в описании
+    каких-либо других ссылок кроме youtube
+    """
+
+    def __call__(self, field_value):
+        # Если поле пустое, пропускаем проверку
+        if not field_value:
+            return
+
+        youtube_patterns = [
+            'https://youtube.com',
+            'https://www.youtube.com',
+            'http://youtube.com',
+            'http://www.youtube.com',
+            'https://youtu.be'
+        ]
+
+        # Проверяем, начинается ли строка с https://youtube.com
+        if not any(field_value.startswith(pattern) for pattern in youtube_patterns):
+            raise ValidationError("Ссылка на видео должна быть с YouTube")
